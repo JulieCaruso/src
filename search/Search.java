37c8985bf8704b78b2d5b438643a6be80db713ea
MarieCharlotteBideaud <mcbideaud@gmail.com>
2015-12-04 15:02:19 +0100

    Search class creation

---
+++ search/Search.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package search;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author MC
+ */
+public class Search {
+    
+    public void vectorialSearch(ArrayList<ArrayList<String>> docsWordsList, ArrayList<String> wordsReq) {
+        // 
+    }
+    
+}
