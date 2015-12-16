0858900f7875f8793bf60d64f9c384189df1a671
JulieCaruso <chevauxenforce@yahoo.fr>
2015-12-16 16:05:57 +0100

    chargement db traitement ailleurs

--- cinemasearch/CinemaSearch.java
+++ cinemasearch/CinemaSearch.java
@@ -26,33 +26,17 @@ public class CinemaSearch {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         // connection base de données
         DbConnection dbConn = new DbConnection();
         dbConn.connectToDb();
         Connection conn = dbConn.getConnection();
-        // création des modèles des tables
-        Documents docModel = new Documents(conn);
-        Mots motModel = new Mots(conn);
-        DocumentMot docMotModel = new DocumentMot(conn);
-        // instanciation des outils
+        
         FileParser p = new FileParser();
         TextualInformation ti = new TextualInformation();
-        TextFrequency tf = new TextFrequency();
         Search s = new Search();
-        // Parsing corpus et empty words
-        ArrayList<Document> Corpus = p.parseCorpus(docModel);
         HashMap<String, String> EmptyWords = p.parseEmptyWords();
-        
-        // Parsing et nettoyage des mots du corpus
-        ArrayList<ArrayList<String>> docsWordsList = ti.generateCorpusWords(Corpus);
-        ArrayList<ArrayList<String>> cleanWordsList = ti.cleanCorpusWords(EmptyWords, docsWordsList);
-     
-        //Ajout des mots du corpus et du lien doc-mot en base de données
-//        ti.insertCorpusWordsInDB(motModel, cleanWordsList);
-//        tf.insertDocMot(motModel, docModel, docMotModel, p.getCorpusTitles(), cleanWordsList);
-        
-        
+
         // essai requete
         String req = "Quelles sont les personnes impliquées dans le film Intouchables?";
         ArrayList<String> rq = ti.parseRequete(EmptyWords, req);
@@ -61,8 +45,29 @@ public class CinemaSearch {
         }
 //        HashMap<String,Double> cosDoc = s.vectorialSearch(p.getCorpusTitles(), rq, docModel, motModel, docMotModel);
 //        System.out.println(cosDoc);
-        
+
         dbConn.disconnectDb(conn);
     }
-    
+
+    public void chargementDonneesDB(Connection conn, HashMap<String, String> EmptyWords, FileParser p, TextualInformation ti) {
+        // création des modèles des tables
+        Documents docModel = new Documents(conn);
+        Mots motModel = new Mots(conn);
+        DocumentMot docMotModel = new DocumentMot(conn);
+        // instanciation des outils
+        
+        TextFrequency tf = new TextFrequency();
+        Search s = new Search();
+        // Parsing corpus et empty words
+        ArrayList<Document> Corpus = p.parseCorpus(docModel);
+
+        // Parsing et nettoyage des mots du corpus
+        ArrayList<ArrayList<String>> docsWordsList = ti.generateCorpusWords(Corpus);
+        ArrayList<ArrayList<String>> cleanWordsList = ti.cleanCorpusWords(EmptyWords, docsWordsList);
+
+        //Ajout des mots du corpus et du lien doc-mot en base de données
+        ti.insertCorpusWordsInDB(motModel, cleanWordsList);
+        tf.insertDocMot(motModel, docModel, docMotModel, p.getCorpusTitles(), cleanWordsList);
+    }
+
 }
