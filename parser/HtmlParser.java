4176785f9a9278aa7e8bb6d034b324288fdab476
MarieCharlotteBideaud <mcbideaud@gmail.com>
2016-01-15 11:34:43 +0100

    New part

---
+++ parser/HtmlParser.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parser;
+
+import java.util.ArrayList;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ *
+ * @author MC
+ */
+public class HtmlParser {
+    private final String requetesFile = "requetes.html";
+    
+    
+    public ArrayList<String> generateWords(Document doc) {
+        ArrayList<String> wordsList = new ArrayList();
+
+        Element body = doc.body();
+        Elements elementsBody = body.getAllElements();
+        for (Element e : elementsBody) {
+            //recup tous les dl et pour chq dl recup les 2 dt a mettre dans liste requete
+        }
+        return wordsList;
+    }
+}
