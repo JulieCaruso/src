ec50dbe741d8f38addbaaa1ba3ea2978874520de
MarieCharlotteBideaud <mcbideaud@gmail.com>
2016-01-18 20:54:43 +0100

    Reformulator : Synonymes OK, Instances commencées

--- cinemasearch/CinemaSearch.java
+++ cinemasearch/CinemaSearch.java
@@ -35,69 +35,75 @@ public class CinemaSearch {
 
         // connection base de données
        /* DbConnection dbConn = new DbConnection();
-        dbConn.connectToDb();
-        Connection conn = dbConn.getConnection();
+         dbConn.connectToDb();
+         Connection conn = dbConn.getConnection();
 
-        FileParser p = new FileParser();
-        TextualInformation ti = new TextualInformation();
-        Evaluator e = new Evaluator();
-        Documents docModel = new Documents(conn);
-        Tf tfModel = new Tf(conn);
-
-        TextFrequency tf = new TextFrequency();
-        Search s = new Search(ti);
-        // Parsing corpus et empty words
-        HashMap<String, String> EmptyWords = p.parseEmptyWords();
-
-       //chargementDonneesDB(conn, EmptyWords, p, ti, tfModel, docModel); */
-
-         // essai html parser 
          FileParser p = new FileParser();
-         HtmlParser hp = new HtmlParser(); 
-         Document d = p.parseFile("requetes.html");
-         hp.generateWords(d);
-         Reformulator r = new Reformulator();
-         r.synonymous("prix");
+         TextualInformation ti = new TextualInformation();
+         Evaluator e = new Evaluator();
+         Documents docModel = new Documents(conn);
+         Tf tfModel = new Tf(conn);
+
+         TextFrequency tf = new TextFrequency();
+         Search s = new Search(ti);
+         // Parsing corpus et empty words
+         HashMap<String, String> EmptyWords = p.parseEmptyWords();
+
+         //chargementDonneesDB(conn, EmptyWords, p, ti, tfModel, docModel); */
+        // essai html parser 
+        FileParser p = new FileParser();
+        HtmlParser hp = new HtmlParser();
+        Document d = p.parseFile("requetes.html");
+        hp.generateWords(d);
+        Reformulator r = new Reformulator();
+        ArrayList<String> synList = new ArrayList<>();
+        ArrayList<String> instList = new ArrayList<>();
+         //synList = r.getSynonymous("prix");
+        //instList = r.getInstance("lieu naissance", "Omar Sy");
+        ArrayList<String> rq = new ArrayList<>();
+        rq.add("prix");
+        rq.add("coucou");
+        synList = r.addSynonymousToReq(rq);
         // essai requete
       /*  String req = "Quelles sont les personnes impliquées dans le film Intouchables?";
-        ArrayList<String> rq = ti.parseRequete(EmptyWords, req);
-        HashMap<String, Double> cosDoc = s.vectorialSearch(docModel.getCorpusTitles(), rq, tfModel, docModel);
-        HashMap<String, Integer> pertDoc = s.pertinence(cosDoc);
-        System.out.println("Taille du corpusDoc : " + docModel.getCorpusTitles().size());
-        System.out.println("Taille de la map cosDoc : " + cosDoc.size());
-        System.out.println("Taille de la map pertDoc : " + pertDoc.size());
-        for (Map.Entry<String, Integer> entry : pertDoc.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
+         ArrayList<String> rq = ti.parseRequete(EmptyWords, req);
+         HashMap<String, Double> cosDoc = s.vectorialSearch(docModel.getCorpusTitles(), rq, tfModel, docModel);
+         HashMap<String, Integer> pertDoc = s.pertinence(cosDoc);
+         System.out.println("Taille du corpusDoc : " + docModel.getCorpusTitles().size());
+         System.out.println("Taille de la map cosDoc : " + cosDoc.size());
+         System.out.println("Taille de la map pertDoc : " + pertDoc.size());
+         for (Map.Entry<String, Integer> entry : pertDoc.entrySet()) {
+         System.out.println(entry.getKey() + ": " + entry.getValue());
+         }
 
-        Double percentage = e.evaluate(pertDoc, 1);
-        DecimalFormat numberFormat = new DecimalFormat("#.00");
-        System.out.println("Pertinence de résultat de la requête : " + numberFormat.format(percentage * 100) + "%");
+         Double percentage = e.evaluate(pertDoc, 1);
+         DecimalFormat numberFormat = new DecimalFormat("#.00");
+         System.out.println("Pertinence de résultat de la requête : " + numberFormat.format(percentage * 100) + "%");
 
-        dbConn.disconnectDb(conn);
-    }
+         dbConn.disconnectDb(conn);
+         }
 
-    public static void chargementDonneesDB(Connection conn, HashMap<String, String> EmptyWords, FileParser p, TextualInformation ti, Tf tfModel, Documents docModel) {
-        // création des modèles des tables
-        Mots motModel = new Mots(conn);
-        DocumentMot docMotModel = new DocumentMot(conn);
-        motModel.truncate();
-        docMotModel.truncate();
-        docModel.truncate();
-        tfModel.truncate();
+         public static void chargementDonneesDB(Connection conn, HashMap<String, String> EmptyWords, FileParser p, TextualInformation ti, Tf tfModel, Documents docModel) {
+         // création des modèles des tables
+         Mots motModel = new Mots(conn);
+         DocumentMot docMotModel = new DocumentMot(conn);
+         motModel.truncate();
+         docMotModel.truncate();
+         docModel.truncate();
+         tfModel.truncate();
 
-        // instanciation des outils
-        TextFrequency tf = new TextFrequency();
-        // Parsing corpus et empty words
-        ArrayList<Document> Corpus = p.parseCorpus(docModel);
+         // instanciation des outils
+         TextFrequency tf = new TextFrequency();
+         // Parsing corpus et empty words
+         ArrayList<Document> Corpus = p.parseCorpus(docModel);
 
-        // Parsing et nettoyage des mots du corpus
-        ArrayList<ArrayList<String>> docsWordsList = ti.generateCorpusWords(Corpus);
-        ArrayList<ArrayList<String>> cleanWordsList = ti.cleanCorpusWords(EmptyWords, docsWordsList);
+         // Parsing et nettoyage des mots du corpus
+         ArrayList<ArrayList<String>> docsWordsList = ti.generateCorpusWords(Corpus);
+         ArrayList<ArrayList<String>> cleanWordsList = ti.cleanCorpusWords(EmptyWords, docsWordsList);
 
-        //Ajout des mots du corpus et du lien doc-mot en base de données
-        //ti.insertCorpusWordsInDB(motModel, cleanWordsList);
-        tf.insertDocMot(motModel, docModel, docMotModel, tfModel, p.getCorpusTitles(), cleanWordsList); */
+         //Ajout des mots du corpus et du lien doc-mot en base de données
+         //ti.insertCorpusWordsInDB(motModel, cleanWordsList);
+         tf.insertDocMot(motModel, docModel, docMotModel, tfModel, p.getCorpusTitles(), cleanWordsList); */
     }
 
 }
--- reformulator/Reformulator.java
+++ reformulator/Reformulator.java
@@ -14,64 +14,90 @@ import sparqlclient.SparqlClient;
  * @author MC
  */
 public class Reformulator {
+
     // Prend en entree une liste de requetes sous forme de mots cles
     // et fournit en sortie la liste des resquetes reformulees
-
     private ArrayList<String> reformReqList = new ArrayList();
 
-    private ArrayList<String> findSynonymous(ArrayList<String> keyWordsReqList) {
-        ArrayList<String> synReqList = new ArrayList();
-
+    public ArrayList<String> addSynonymousToReq(ArrayList<String> keyWordsReqList) {
+        //copie des mots de la requete dans la liste qui va etre retournee
+        ArrayList<String> synReqList = new ArrayList(keyWordsReqList);
+        
+        // recherche des synonymes
+        ArrayList<String> synList = new ArrayList();
+        for (String word : keyWordsReqList) {
+            synList.addAll(getSynonymous(word));
+        }
+        
+        // construction de la liste retournee en ajoutant les synonymes a la liste de mots de depart
+        for (String syn : synList) {
+            if (synReqList.contains(syn) == false) {
+                synReqList.add(syn);
+            }
+        }
+        System.out.println(synReqList.toString());
         return synReqList;
     }
 
     // -----------------------------   SPARQL PART  -------------------------------- //
-    public void synonymous(String word) {
+    public ArrayList<String> getSynonymous(String word) {
+
+        ArrayList<String> synList = new ArrayList<>();
         SparqlClient sparqlClient = new SparqlClient("localhost:3030/space");
 
-        // Pour les instances : fonctionne
-        /*String query = "PREFIX : <http://ontologies.alwaysdata.net/space#>"
-                + "PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
-                + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
-                + "SELECT ?labels WHERE { ?propriete rdfs:label \"lieu naissance\"@fr."
-                + "?entite rdfs:label \"Omar Sy\"."
-                + "  ?entite ?propriete ?res."
-                + "  ?res rdfs:label ?labels."
-                + "}";
-        Iterable<Map<String, String>> results = sparqlClient.select(query);
-        // nbPersonnesParPiece(sparqlClient);;
-        for (Map<String, String> m : results) {
-            System.out.println(m.toString());
-        }*/
         String query = "PREFIX : <http://ontologies.alwaysdata.net/space#>"
                 + "PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
                 + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
-                + "SELECT ?label"
+                + "SELECT ?label "
                 + "WHERE {"
                 + "?entite rdfs:label \"" + word + "\"@fr."
                 + "?entite rdfs:label ?label."
                 + "}";
         Iterable<Map<String, String>> results = sparqlClient.select(query);
-        // nbPersonnesParPiece(sparqlClient);;
         for (Map<String, String> m : results) {
-            System.out.println(m.toString());
+            synList.add(m.get("label"));
         }
-        //--------------- Affiche bien 5 resultats mais vides... --------------
+        //System.out.println(synList.toString());
+        return synList;
     }
 
-    
-    
-    /*private static void nbPersonnesParPiece(SparqlClient sparqlClient) {
-        String query = "PREFIX : <http://www.lamaisondumeurtre.fr#>\n"
-                + "SELECT ?piece (COUNT(?personne) AS ?nbPers) WHERE\n"
-                + "{\n"
-                + "    ?personne :personneDansPiece ?piece.\n"
-                + "}\n"
-                + "GROUP BY ?piece\n";
+    // Exemple : "lieu de naissance, Omar Sy" retourne "Trappes"
+    public ArrayList<String> getInstance(String propriete, String entite) {
+        ArrayList<String> instList = new ArrayList<>();
+        SparqlClient sparqlClient = new SparqlClient("localhost:3030/space");
+
+        // Pour les instances : fonctionne
+        String query = "PREFIX : <http://ontologies.alwaysdata.net/space#>"
+                + "PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+                + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+                + "SELECT ?labels "
+                + "WHERE {"
+                + "?propriete rdfs:label \"" + propriete + "\"@fr."
+                + "?entite rdfs:label \"" + entite + "\"."
+                + "?entite ?propriete ?res."
+                + "?res rdfs:label ?labels."
+                + "}";
         Iterable<Map<String, String>> results = sparqlClient.select(query);
-        System.out.println("nombre de personnes par pièce:");
-        for (Map<String, String> result : results) {
-            System.out.println(result.get("piece") + " : " + result.get("nbPers"));
+
+        for (Map<String, String> m : results) {
+            instList.add(m.get("labels"));
         }
-    }*/
+        //System.out.println(instList.toString());
+        return instList;
+    }
+
+
+    /*private static void nbPersonnesParPiece(SparqlClient sparqlClient) {
+     String query = "PREFIX : <http://www.lamaisondumeurtre.fr#>\n"
+     + "SELECT ?piece (COUNT(?personne) AS ?nbPers) WHERE\n"
+     + "{\n"
+     + "    ?personne :personneDansPiece ?piece.\n"
+     + "}\n"
+     + "GROUP BY ?piece\n";
+     Iterable<Map<String, String>> results = sparqlClient.select(query);
+     System.out.println("nombre de personnes par pièce:");
+     for (Map<String, String> result : results) {
+     System.out.println(result.get("piece") + " : " + result.get("nbPers"));
+     }
+     }*/
 }
