c0de6a88cd12822b575bf88f0b888ae01a257b77
MarieCharlotteBideaud <mcbideaud@gmail.com>
2016-01-20 19:16:11 +0100

    Req a tester

--- cinemasearch/CinemaSearch.java
+++ cinemasearch/CinemaSearch.java
@@ -34,79 +34,83 @@ public class CinemaSearch {
     public static void main(String[] args) {
 
         // connection base de données
-       /* DbConnection dbConn = new DbConnection();
-         dbConn.connectToDb();
-         Connection conn = dbConn.getConnection();
+        DbConnection dbConn = new DbConnection();
+        dbConn.connectToDb();
+        Connection conn = dbConn.getConnection();
 
-         FileParser p = new FileParser();
-         TextualInformation ti = new TextualInformation();
-         Evaluator e = new Evaluator();
-         Documents docModel = new Documents(conn);
-         Tf tfModel = new Tf(conn);
-
-         TextFrequency tf = new TextFrequency();
-         Search s = new Search(ti);
-         // Parsing corpus et empty words
-         HashMap<String, String> EmptyWords = p.parseEmptyWords();
-
-         //chargementDonneesDB(conn, EmptyWords, p, ti, tfModel, docModel); */
-        // essai html parser 
         FileParser p = new FileParser();
+        TextualInformation ti = new TextualInformation();
+        Evaluator e = new Evaluator();
+        Documents docModel = new Documents(conn);
+        Tf tfModel = new Tf(conn);
+
+        TextFrequency tf = new TextFrequency();
+        Search s = new Search(ti);
+        // Parsing corpus et empty words
+        HashMap<String, String> EmptyWords = p.parseEmptyWords();
+        //chargementDonneesDB(conn, EmptyWords, p, ti, tfModel, docModel);  
         HtmlParser hp = new HtmlParser();
         Document d = p.parseFile("requetes.html");
-        hp.generateWords(d);
+        ArrayList<String> rq = hp.generateWords(d);
+        System.out.println("avant : " + rq.toString());
         Reformulator r = new Reformulator();
+        ArrayList<String> refReqList = new ArrayList<>();
+        refReqList = r.reformulate(rq);
+        
         ArrayList<String> synList = new ArrayList<>();
+        synList = r.getSynonymous("prix");
         ArrayList<String> instList = new ArrayList<>();
-         //synList = r.getSynonymous("prix");
-        //instList = r.getInstance("lieu naissance", "Omar Sy");
+        instList = r.getInstance("lieu naissance", "Omar Sy");
         ArrayList<String> rq = new ArrayList<>();
         rq.add("Omar Sy");
         rq.add("lieu naissance");
+        rq.add("Julie Caruso");
         rq.add("coucou");
-        synList = r.addSynonymousToReq(rq);
-        instList = r.addInstancesToReq(rq);
-        
+        rq.add("a pour membre de jury");
+        refReqList = r.reformulate(rq);
+        System.out.println("avant : " + rq.toString());
+        System.out.println("apres : " + refReqList.toString());
         // essai requete
-      /*  String req = "Quelles sont les personnes impliquées dans le film Intouchables?";
-         ArrayList<String> rq = ti.parseRequete(EmptyWords, req);
-         HashMap<String, Double> cosDoc = s.vectorialSearch(docModel.getCorpusTitles(), rq, tfModel, docModel);
-         HashMap<String, Integer> pertDoc = s.pertinence(cosDoc);
-         System.out.println("Taille du corpusDoc : " + docModel.getCorpusTitles().size());
-         System.out.println("Taille de la map cosDoc : " + cosDoc.size());
-         System.out.println("Taille de la map pertDoc : " + pertDoc.size());
-         for (Map.Entry<String, Integer> entry : pertDoc.entrySet()) {
-         System.out.println(entry.getKey() + ": " + entry.getValue());
-         }
+        String req = "Quelles sont les personnes impliquées dans le film Intouchables?";
+        ArrayList<String> rq = ti.parseRequete(EmptyWords, req);
 
-         Double percentage = e.evaluate(pertDoc, 1);
-         DecimalFormat numberFormat = new DecimalFormat("#.00");
-         System.out.println("Pertinence de résultat de la requête : " + numberFormat.format(percentage * 100) + "%");
+        HashMap< String, Double> cosDoc = s.vectorialSearch(docModel.getCorpusTitles(), rq, tfModel, docModel);
+        HashMap<String, Integer> pertDoc = s.pertinence(cosDoc);
+        System.out.println("Taille du corpusDoc : " + docModel.getCorpusTitles().size());
+        System.out.println("Taille de la map cosDoc : " + cosDoc.size());
+        System.out.println("Taille de la map pertDoc : " + pertDoc.size());
+        for (Map.Entry<String, Integer> entry : pertDoc.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
 
-         dbConn.disconnectDb(conn);
-         }
+        Double percentage = e.evaluate(pertDoc, 1);
+        DecimalFormat numberFormat = new DecimalFormat("#.00");
+        System.out.println("Pertinence de résultat de la requête : " + numberFormat.format(percentage * 100) + "%");
 
-         public static void chargementDonneesDB(Connection conn, HashMap<String, String> EmptyWords, FileParser p, TextualInformation ti, Tf tfModel, Documents docModel) {
-         // création des modèles des tables
-         Mots motModel = new Mots(conn);
-         DocumentMot docMotModel = new DocumentMot(conn);
-         motModel.truncate();
-         docMotModel.truncate();
-         docModel.truncate();
-         tfModel.truncate();
+        dbConn.disconnectDb(conn);
+    }
 
-         // instanciation des outils
-         TextFrequency tf = new TextFrequency();
-         // Parsing corpus et empty words
-         ArrayList<Document> Corpus = p.parseCorpus(docModel);
+    public static void chargementDonneesDB(Connection conn, HashMap<String, String> EmptyWords, FileParser p, TextualInformation ti, Tf tfModel, Documents docModel) {
+        // création des modèles des tables
+        Mots motModel = new Mots(conn);
+        DocumentMot docMotModel = new DocumentMot(conn);
+        motModel.truncate();
+        docMotModel.truncate();
+        docModel.truncate();
+        tfModel.truncate();
 
-         // Parsing et nettoyage des mots du corpus
-         ArrayList<ArrayList<String>> docsWordsList = ti.generateCorpusWords(Corpus);
-         ArrayList<ArrayList<String>> cleanWordsList = ti.cleanCorpusWords(EmptyWords, docsWordsList);
+        // instanciation des outils
+        TextFrequency tf = new TextFrequency();
+        // Parsing corpus et empty words
+        ArrayList<Document> Corpus = p.parseCorpus(docModel);
 
-         //Ajout des mots du corpus et du lien doc-mot en base de données
-         //ti.insertCorpusWordsInDB(motModel, cleanWordsList);
-         tf.insertDocMot(motModel, docModel, docMotModel, tfModel, p.getCorpusTitles(), cleanWordsList); */
+        // Parsing et nettoyage des mots du corpus
+        ArrayList<ArrayList<String>> docsWordsList = ti.generateCorpusWords(Corpus);
+        ArrayList<ArrayList<String>> cleanWordsList = ti.cleanCorpusWords(EmptyWords, docsWordsList);
+
+        //Ajout des mots du corpus et du lien doc-mot en base de données
+        //ti.insertCorpusWordsInDB(motModel, cleanWordsList);
+        tf.insertDocMot(motModel, docModel, docMotModel, tfModel, p.getCorpusTitles(), cleanWordsList);
     }
 
 }
--- reformulator/Reformulator.java
+++ reformulator/Reformulator.java
@@ -19,6 +19,31 @@ public class Reformulator {
     // et fournit en sortie la liste des resquetes reformulees
     private ArrayList<String> reformReqList = new ArrayList();
 
+    public ArrayList<String> reformulate(ArrayList<String> keyWordsReqList) {
+        // copie des mots de la requete dans la liste qui va etre retournee
+        ArrayList<String> refReqList = new ArrayList(keyWordsReqList);
+        ArrayList<String> synReqList = new ArrayList<>();
+        ArrayList<String> instReqList = new ArrayList<>();
+
+        // ajout des synonymes
+        synReqList = addSynonymousToReq(keyWordsReqList);
+        for (String syn : synReqList) {
+            if (refReqList.contains(syn) == false) {
+                refReqList.add(syn);
+            }
+        }
+
+        // ajout des instances
+        instReqList = addInstancesToReq(keyWordsReqList);
+        for (String inst : instReqList) {
+            if (refReqList.contains(inst) == false) {
+                refReqList.add(inst);
+            }
+        }
+
+        return refReqList;
+    }
+
     public ArrayList<String> addSynonymousToReq(ArrayList<String> keyWordsReqList) {
         // copie des mots de la requete dans la liste qui va etre retournee
         ArrayList<String> synReqList = new ArrayList(keyWordsReqList);
@@ -35,7 +60,7 @@ public class Reformulator {
                 synReqList.add(syn);
             }
         }
-        System.out.println(synReqList.toString());
+        //System.out.println(synReqList.toString());
         return synReqList;
     }
 
@@ -44,32 +69,36 @@ public class Reformulator {
         // copie des mots de la requete dans la liste qui va etre retournee
         ArrayList<String> instReqList = new ArrayList(keyWordsReqList);
 
-        String property = null;
-        String entity = null;
-        ArrayList<String> instList = new ArrayList();
+        ArrayList<String> property = new ArrayList<>();;
+        ArrayList<String> entity = new ArrayList<>();;
+        ArrayList<String> instList = new ArrayList<>();
 
-        // si la requete contient exactement 2 mots
-        if (keyWordsReqList.size() == 2) {
-            // si l'un d'eux est une propriete
-            property = findProperty(keyWordsReqList);
-            if (property != null) {
-                // si l'autre est une entite
-                entity = findEntity(keyWordsReqList);
-                if (entity != null) {
-                    // alors on cherche l'instance (les instances) a ajouter a la requete
-                    instList = getInstance(property, entity);
-
-                    // enrichissement de la requete
-                    for (String inst : instList) {
-                        if (instReqList.contains(inst) == false) {
-                            instReqList.add(inst);
+        // recherche des proprietes
+        property = findProperty(keyWordsReqList);
+        //System.out.println("proprietes : " + property);
+        if (property != null) {
+            // recherche des entites
+            entity = findEntity(keyWordsReqList);
+            //System.out.println("entites : " + entity);
+            if (entity != null) {
+                // alors on cherche les instances a ajouter a la requete
+                for (String e : entity) {
+                    for (String p : property) {
+                        instList = getInstance(p, e);
+                        //System.out.println("instances : " + instList);
+                        // enrichissement de la requete
+                        for (String inst : instList) {
+                            if (instReqList.contains(inst) == false) {
+                                instReqList.add(inst);
+                            }
                         }
                     }
                 }
+
             }
         }
 
-        System.out.println(instReqList.toString());
+        //System.out.println(instReqList.toString());
         return instReqList;
     }
 
@@ -79,6 +108,7 @@ public class Reformulator {
         ArrayList<String> synList = new ArrayList<>();
         SparqlClient sparqlClient = new SparqlClient("localhost:3030/space");
 
+        // avec @fr
         String query = "PREFIX : <http://ontologies.alwaysdata.net/space#>"
                 + "PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
                 + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
@@ -91,6 +121,35 @@ public class Reformulator {
         for (Map<String, String> m : results) {
             synList.add(m.get("label"));
         }
+        
+        // avec @en
+        String query2 = "PREFIX : <http://ontologies.alwaysdata.net/space#>"
+                + "PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+                + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+                + "SELECT ?label "
+                + "WHERE {"
+                + "?entite rdfs:label \"" + word + "\"@en."
+                + "?entite rdfs:label ?label."
+                + "}";
+        Iterable<Map<String, String>> results2 = sparqlClient.select(query2);
+        for (Map<String, String> m : results2) {
+            synList.add(m.get("label"));
+        }
+        
+        // avec aucun tag de langue
+        String query3 = "PREFIX : <http://ontologies.alwaysdata.net/space#>"
+                + "PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+                + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+                + "SELECT ?label "
+                + "WHERE {"
+                + "?entite rdfs:label \"" + word + "\"."
+                + "?entite rdfs:label ?label."
+                + "}";
+        Iterable<Map<String, String>> results3 = sparqlClient.select(query3);
+        for (Map<String, String> m : results3) {
+            synList.add(m.get("label"));
+        }
+        
         //System.out.println(synList.toString());
         return synList;
     }
@@ -116,14 +175,31 @@ public class Reformulator {
         for (Map<String, String> m : results) {
             instList.add(m.get("labels"));
         }
-        //System.out.println(instList.toString());
+
+        // dans l'autre sens
+        String query2 = "PREFIX : <http://ontologies.alwaysdata.net/space#>"
+                + "PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+                + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+                + "SELECT ?labels "
+                + "WHERE {"
+                + "?propriete rdfs:label \"" + propriete + "\"@fr."
+                + "?entite rdfs:label \"" + entite + "\"."
+                + "?res ?propriete ?entite."
+                + "?res rdfs:label ?labels."
+                + "}";
+        Iterable<Map<String, String>> results2 = sparqlClient.select(query2);
+
+        for (Map<String, String> m : results2) {
+            instList.add(m.get("labels"));
+        }
+        //System.out.println("instances " + instList.toString());
         return instList;
     }
 
-    // Si l'un des termes de la requete est une propriete, alors il est retourne (null sinon)
-    public String findProperty(ArrayList<String> keyWordsReqList) {
+    // Si un ou plusieurs termes de la requete sont des proprietes, alors il sont retournes
+    public ArrayList<String> findProperty(ArrayList<String> keyWordsReqList) {
         SparqlClient sparqlClient = new SparqlClient("localhost:3030/space");
-        String property = null;
+        ArrayList<String> property = new ArrayList<>();
         for (String keyWord : keyWordsReqList) {
             // System.out.println("key :" + keyWord);
             String query = "PREFIX : <http://ontologies.alwaysdata.net/space#>"
@@ -135,17 +211,17 @@ public class Reformulator {
                     + "}";
             boolean found = sparqlClient.ask(query);
             if (found) {
-                property = keyWord;
+                property.add(keyWord);
             }
         }
         // System.out.println(property);
         return property;
     }
 
-    // Si l'un des termes de la requete est une propriete, alors il est retourne (null sinon)
-    public String findEntity(ArrayList<String> keyWordsReqList) {
+    // Si l'un des termes de la requete est une entite, alors il est retourne (null sinon)
+    public ArrayList<String> findEntity(ArrayList<String> keyWordsReqList) {
         SparqlClient sparqlClient = new SparqlClient("localhost:3030/space");
-        String entity = null;
+        ArrayList<String> entity = new ArrayList<>();
         for (String keyWord : keyWordsReqList) {
             // System.out.println("key :" + keyWord);
             String query = "PREFIX : <http://ontologies.alwaysdata.net/space#>"
@@ -157,7 +233,7 @@ public class Reformulator {
                     + "}";
             boolean found = sparqlClient.ask(query);
             if (found) {
-                entity = keyWord;
+                entity.add(keyWord);
             }
         }
         // System.out.println(entity);
